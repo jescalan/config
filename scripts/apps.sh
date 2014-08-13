0e34d07f71695418c9e9f4ec31f460c7fc6e5324
Jeff Escalante <hello@jenius.me>
2014-08-13 14:31:54 -0400

    changes to installed apps
    - remove appzapper
    - add dropbox
    - add gitx
    - add sketch-toolbox
    - add torpedo

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -4,26 +4,30 @@ function -() {
 
 brew update
 
-- adium
 - alfred
-- appzapper
+- bartender
 - caffeine
 - cloudapp
 - cloudup
 - daisydisk
 - dash
+- dropbox
 - flux
 - fantastical
 - ffmpegx
+- gitx
 - google-chrome
 - imagealpha
 - imageoptim
 - node-webkit
+- rescuetime
 - sequel-pro
 - silverlight
 - sketch
+- sketch-toolbox
 - skype
 - slack
 - spotify
+- torpedo
 - totalterminal
 - vlc
